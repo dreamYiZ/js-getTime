f788bb7d4438f058cd44fcbf0984d15dab6f876c
rentoo <dreamyiz@outlook.com>
2020-12-17 16:56:45 +0800

    -

--- getTime.js
+++ getTime.js
@@ -9,3 +9,15 @@ function getTime(h, m, s){
 
 console.log('getTime(-8)', getTime(-8));
 console.log('getTime(0, -15)', getTime(0, -15));
+
+
+getResponseTime({
+    start: getTime(-8, -15),
+    end: getTime(-8),
+    step: 'MINUTE',
+    normal : true,
+    scope :'Service',
+    serviceName : 'ylkj-upms' ,
+    name : 'service_cpm'
+    // serviceId : this.serviceID,
+})
